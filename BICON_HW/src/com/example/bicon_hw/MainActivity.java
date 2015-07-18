66c3ea24f7e95729804cd7189e1bdf9e4c45d30e
SongJongMun <skuugioge@daum.net>
2015-07-18 11:48:18 +0900

    BICON_HW
    Hanium Big Project

---
+++ BICON_HW/src/com/example/bicon_hw/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.bicon_hw;
+
+import com.radiusnetworks.ibeacon.IBeaconManager;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		verifyBluetooth();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+	public void onMonitoringClicked(View view) {
+		Toast.makeText(getApplicationContext(), "비콘 검색을 시작합니다.", Toast.LENGTH_LONG).show();
+		Intent myIntent = new Intent(this, MonitoringActivity.class);
+		this.startActivity(myIntent);
+	}
+
+	private void verifyBluetooth() {
+		try {
+			/** 
+			 * 블루투스를 사용하지 못하는 경우 - 
+			 * An accessor for the singleton instance of this class, Check if Bluetooth LE is supported by this Android device 
+			 * **/
+			if (!IBeaconManager.getInstanceForApplication(this).checkAvailability()) {
+				final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+				builder.setTitle("Bluetooth not enabled");
+				builder.setMessage("Please enable bluetooth in settings and restart this application.");
+				builder.setPositiveButton(android.R.string.ok, null);
+				builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+					@Override
+					public void onDismiss(DialogInterface dialog) {
+						finish();
+						System.exit(0);
+					}
+				});
+				builder.show();
+			}
+			/** BLE 기능을 지원하지 않는 운영체제일 경우 **/
+		} catch (RuntimeException e) {
+			final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			builder.setTitle("Bluetooth LE not available");
+			builder.setMessage("Sorry, this device does not support Bluetooth LE.");
+			builder.setPositiveButton(android.R.string.ok, null);
+			builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+				@Override
+				public void onDismiss(DialogInterface dialog) {
+					finish();
+					System.exit(0);
+				}
+			});
+			builder.show();
+		}
+	}
+}
